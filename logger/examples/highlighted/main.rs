4d1382cea919b49cc42466d03c36c564308bf601
altacountbabi <altacountbabi@users.noreply.github.com>
2024-11-30 13:21:43 +0200

    Fix "highlight" example in logger crate

--- logger/examples/highlighted/main.rs
+++ logger/examples/highlighted/main.rs
@@ -1,5 +1,5 @@
 use logger::{warn, Colorize, Location};
 
 fn main() {
-    warn!("Using new line in println! call", location: Location::new_with_section("examples/highlighted/bad_code.rs", 2..=2, 21..=22), hint: format!("Remove {} in {} call", "\\n".bold(), "println!".bold()));
+    warn!("Using new line in println! call", location: Location::new_with_section(Some("examples/highlighted/bad_code.rs".into()), 1..=1, 21..=22), hint: format!("Remove {} in {} call", "\\n".bold(), "println!".bold()));
 }
