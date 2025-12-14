d6146af2fba99bcd4cbba1b2d86b4ebbca344b66
altacountbabi <altacountbabi@users.noreply.github.com>
2025-12-14 22:47:57 +0200

    Fix `map` function in stdlib and add `any` function

--- src/runtime/stdlib/lists.rs
+++ src/runtime/stdlib/lists.rs
@@ -1,24 +1,45 @@
-use crate::runtime::{Value, types::builtin};
+use crate::runtime::{Value, ValueKind, types::builtin};
 use std::collections::HashMap;
 
 pub(super) fn lists(map: &mut HashMap<String, Value>) {
     map.insert(
         "map".to_owned(),
         builtin(|ctx| {
-            let args_len = 3;
-            let cond = ctx.get_arg(0, args_len)?;
-            let then_branch = ctx.get_arg(1, args_len)?;
-            let else_branch = ctx.get_arg(2, args_len)?;
+            let args_len = 2;
+            let list = ctx.ensure_is_array(ctx.get_arg_evaluated(0, args_len)?)?;
+            let callback = ctx.ensure_is_function(ctx.get_arg_evaluated(1, args_len)?)?;
+            let (_, arg, expr) = callback.data;
 
-            let scope = ctx.new_scope();
+            let mut result = Vec::new();
 
-            let cond = scope.eval_expr(&cond)?;
-
-            if cond.is_truthy() {
-                return scope.eval_expr(&then_branch);
+            for item in &list.data {
+                let scope = ctx.call_site.create_scope(expr.clone());
+                scope.define(arg.clone(), item.clone());
+                result.push(scope.eval()?);
             }
 
-            scope.eval_expr(&else_branch)
+            Ok(Value::new(ValueKind::Array(result), ctx.expr.span))
+        }),
+    );
+
+    map.insert(
+        "any".to_owned(),
+        builtin(|ctx| {
+            let args_len = 2;
+            let list = ctx.ensure_is_array(ctx.get_arg_evaluated(0, args_len)?)?;
+            let callback = ctx.ensure_is_function(ctx.get_arg_evaluated(1, args_len)?)?;
+            let (_, arg, expr) = callback.data;
+
+            for item in &list.data {
+                let scope = ctx.call_site.create_scope(expr.clone());
+                scope.define(arg.clone(), item.clone());
+                let res = scope.eval()?;
+                if res.is_truthy() {
+                    return Ok(Value::new(ValueKind::Boolean(true), ctx.expr.span));
+                }
+            }
+
+            Ok(Value::new(ValueKind::Boolean(false), ctx.expr.span))
         }),
     );
 }
